89af09f2303312ac94057959c6c539eb97538a2f
dmitry <dima.ovsyanyk@gmail.com>
2021-08-22 16:01:56 +0300

    add openapi generation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,16 @@ rootProject.name = "fintrack"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openApiVersion: String by settings
+
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
+
+        id("org.openapi.generator") version openApiVersion
     }
 }
 
 include("common")
 include("m2l2-testing")
+include("m3-transport-main-openapi")
