dac013cae8496908c75f8faf7eec526c3351531a
dmitry <dima.ovsyanyk@gmail.com>
2021-09-04 16:05:25 +0300

    spring init

---
+++ m4-frameworks-spring-app/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,9 @@ rootProject.name = "fintrack"
 pluginManagement {
     val kotlinVersion: String by settings
     val openApiVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyVersion: String by settings
+    val springPluginVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
@@ -10,6 +13,10 @@ pluginManagement {
         kotlin("plugin.serialization") version kotlinVersion
 
         id("org.openapi.generator") version openApiVersion
+
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyVersion
+        kotlin("plugin.spring") version springPluginVersion
     }
 }
 
@@ -20,3 +27,4 @@ include("m3-transport-main-openapi")
 include("m3-transport-mapping-openapi")
 include("m3-transport-main-mp")
 include("m3-transport-mapping-mp")
+include("m4-frameworks-spring-app")
