4d1e836d6d46f34eeb224103f77a70f597cf1138
dmitry <dima.ovsyanyk@gmail.com>
2021-09-05 13:45:29 +0300

    setup spring app

--- m4-frameworks-app-spring/build.gradle.kts
+++ m4-frameworks-app-spring/build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation(project(":m3-common"))
+    implementation(project(":m3-transport-main-openapi"))
+    implementation(project(":m3-transport-mapping-openapi"))
+    implementation(project(":m4-stubs"))
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
