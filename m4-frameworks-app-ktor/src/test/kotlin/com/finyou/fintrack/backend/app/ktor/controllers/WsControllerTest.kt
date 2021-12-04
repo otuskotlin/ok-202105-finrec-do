ffaf640055243a78e21ff5054ee22e257a5db759
dmitry <dima.ovsyanyk@gmail.com>
2021-12-04 18:26:50 +0200

    webSocket implementation testing

---
+++ m4-frameworks-app-ktor/src/test/kotlin/com/finyou/fintrack/backend/app/ktor/controllers/WsControllerTest.kt
@@ -0,0 +1,35 @@
+package com.finyou.fintrack.backend.app.ktor.controllers
+
+import com.finyou.fintrack.backend.app.ktor.mapper
+import com.finyou.fintrack.backend.app.ktor.module
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.finyou.fintrack.openapi.models.*
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+import io.ktor.http.cio.websocket.*
+import io.ktor.server.testing.*
+
+class WsControllerTest : FunSpec({
+    test("webSocket") {
+        withTestApplication({ module(testing = true) }) {
+            handleWebSocketConversation("/ws") { incoming, outgoing ->
+                run {
+                    val responseFrame = incoming.receive()
+                    val response = mapper.readValue<BaseMessage>(responseFrame.readBytes())
+                    response.shouldBeInstanceOf<InitTransactionResponse>()
+                }
+
+                val request = CreateTransactionRequest(
+                    createTransaction = creatableTransaction,
+                    debug = stub
+                )
+                val requestFrame = Frame.Text(mapper.writeValueAsString(request))
+                outgoing.send(requestFrame)
+
+                val responseFrame = incoming.receive()
+                val response = mapper.readValue<BaseMessage>(responseFrame.readBytes())
+                response.shouldBeInstanceOf<CreateTransactionResponse>()
+            }
+        }
+    }
+})
